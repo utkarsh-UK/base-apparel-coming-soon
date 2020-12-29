d9c6d37e5536a6806177616e79c8fec911eb6ad7
utkarsh-UK <utkarshkore98@gmail.com>
2020-12-29 14:54:28 +0530

    Completed desktop version

--- scripts/index.js
+++ scripts/index.js
@@ -5,10 +5,45 @@ btnSubmit.addEventListener("click", (event) => {
   const email = document.getElementById("email").value;
 
   if (!email) {
-    document.getElementsByClassName("error-id")[0].classList.remove("error-img-hidden");
+    document
+      .getElementsByClassName("error-id")[0]
+      .classList.remove("error-img-hidden");
     document.getElementsByClassName("error-id")[0].classList.add("error-img");
-  } else if(!email.includes("@")) {
     document.getElementsByClassName("info")[0].classList.remove("error-hidden");
     document.getElementsByClassName("info")[0].classList.add("error");
+    document
+      .getElementsByClassName("subscription-form")[0]
+      .classList.remove("normal-border");
+    document
+      .getElementsByClassName("subscription-form")[0]
+      .classList.add("error-border");
+  } else if (!email.includes("@")) {
+    document
+      .getElementsByClassName("error-id")[0]
+      .classList.remove("error-img-hidden");
+    document.getElementsByClassName("error-id")[0].classList.add("error-img");
+    document.getElementsByClassName("info")[0].classList.remove("error-hidden");
+    document.getElementsByClassName("info")[0].classList.add("error");
+    document
+      .getElementsByClassName("subscription-form")[0]
+      .classList.remove("normal-border");
+    document
+      .getElementsByClassName("subscription-form")[0]
+      .classList.add("error-border");
+  } else {
+    document
+      .getElementsByClassName("error-id")[0]
+      .classList.remove("error-img");
+    document
+      .getElementsByClassName("error-id")[0]
+      .classList.add("error-img-hidden");
+    document.getElementsByClassName("info")[0].classList.remove("error");
+    document.getElementsByClassName("info")[0].classList.add("error-hidden");
+    document
+      .getElementsByClassName("subscription-form")[0]
+      .classList.remove("error-border");
+    document
+      .getElementsByClassName("subscription-form")[0]
+      .classList.add("normal-border");
   }
 });
